c81fef9b0d7829d036c8d0bbf4cdf8acac234d2d
bjjk <86003500+bjjk@users.noreply.github.com>
2021-06-29 12:35:34 +0300

    homework

--- pythonfile.py
+++ pythonfile.py
@@ -2,3 +2,14 @@
 # Если число делится на 3 без остатка - написать Fizz
 # Если число делится на 5 без остатка - написать Buzz
 # Если число делится на 3 и на 5 без остатка - написать FizzBuzz
+
+for num in range(0, 100):
+    if num % 3 == 0 and num % 5 == 0:
+        print(num, "FizzBuzz")
+    if num % 3 == 0:
+        print(num, "Fizz")
+    elif num % 5 == 0:
+        print(num, "Buzz")
+
+
+
